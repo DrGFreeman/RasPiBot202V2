2e4f769eb8845b224e9b8a2acbb7c99250d9f908
DrGFreeman <jul.dlbt@gmail.com>
2017-04-22 22:56:35 -0400

    Added preliminary python AStar class for testing

---
+++ pi/AStar.py
@@ -0,0 +1,36 @@
+import smbus
+import struct
+import time
+
+class AStar:
+    def __init__(self):
+        self.bus = smbus.SMBus(1)
+
+    def read_unpack(self, address, size, format):
+        self.bus.write_byte(20, address)
+        time.sleep(0.0001)
+        byte_list = [self.bus.read_byte(20) for _ in range(size)]
+        return struct.unpack(format, bytes(byte_list))
+
+    def write_pack(self, address, format, *data):
+        data_array = list(struct.pack(format, *data))
+        self.bus.write_i2c_block_data(20, address, data_array)
+        time.sleep(0.0001)
+
+    def leds(self, red, yellow, green):
+        self.write_pack(0, 'BBB', red, yellow, green)
+
+    def read_buttons(self):
+        return self.read_unpack(3, 3, "???")
+
+    def move(self, fwd, turn):
+        self.write_pack(6, 'hh', fwd, turn * 1000)
+
+    def odometer(self):
+        return self.read_unpack(10, 6, 'hhh')
+
+    def read_battery_millivolts(self):
+        return self.read_unpack(16, 2, "H")[0]
+
+    def play_notes(self, notes):
+        self.write_pack(18, 'B15s', 1, notes.encode("ascii"))
